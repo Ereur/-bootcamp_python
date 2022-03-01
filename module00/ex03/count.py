17832805ba9bc75e301b6e3e293e2d6d0646d0ec
Ereur <amoussaouianas@gmail.com>
2022-03-01 14:38:15 +0100

    working with functions ex03

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -6,18 +6,20 @@
 #    By: aamoussa <aamoussa@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/02/28 17:39:46 by aamoussa          #+#    #+#              #
-#    Updated: 2022/02/28 18:56:27 by aamoussa         ###   ########.fr        #
+#    Updated: 2022/03/01 14:36:54 by aamoussa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import sys
 def text_analyzer(str = 'Default') :
     "This function counts the number of upper characters, lower characters, punctuation and spaces in a given text."
     lower_case = 0
     upper_case = 0
     spaces = 0
     i = 0
+    punctuation = 0
     if (str == 'Default'):
-        print('What is the text to analyze?')
+        str = input("What is the text to analyze?\n")
         
     if (type(str) == int) :
         print('argument is not a string')
@@ -33,9 +35,14 @@ def text_analyzer(str = 'Default') :
         elif ((ord(str[i]) < 48 and ord(str[i]) > 32) or ord(str[i]) < 65 and ord(str[i]) > 57):
             punctuation = punctuation + 1
         i = i + 1
+    print('The text contains',upper_case + lower_case + spaces + punctuation ,'character(s):\n')
     print('-', upper_case ,'upper letter(s) \n')
     print('-', lower_case ,'lower letter(s) \n')
     print('-', spaces ,'spaces letter(s) \n')
     print('-', punctuation ,'punctuation letter(s) \n')
 
-        
+if (len(sys.argv) > 2) :
+    print('there is more than one argument')
+    quit()
+if (len(sys.argv) > 1) :
+    text_analyzer(sys.argv[1])
